8db436b5252c04d047c37fdaba2dccfe5abfb116
mozerfucker <tokarevnikita08@gmail.com>
2021-04-14 20:43:42 +0300

    upgrade

--- FunctionalProgramming/lw4_4.9/main.lisp
+++ FunctionalProgramming/lw4_4.9/main.lisp
@@ -2,6 +2,20 @@
 	(member ch '(#\Space #\Tab #\Newline))
 )
 
+
+(defun russian-upper-case-p (char)
+  (position char "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"))
+
+(defun russian-char-downcase (char)
+	(let ((i (russian-upper-case-p char)))
+    (if i 
+        (char "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" i)
+        (char-downcase char))))  
+
+(defun russian-char-equal (char1 char2)
+  (char-equal (russian-char-downcase char1)
+              (russian-char-downcase char2)))
+
 (defun endSentence-char(ch)
 	(member ch '(#\! #\? #\. #\"))
 )
@@ -15,7 +29,7 @@
 		
 		(loop for i from (- (length str) 1) downto 0 do
 			(setq cur-ch (char str i))	
-			(if (and (char-equal ch cur-ch) (not (endSentence-char cur-ch)))
+			(if (and (or (char-equal ch cur-ch) (russian-char-equal ch cur-ch)) (not (endSentence-char cur-ch)))
 				(setq res (+ res 1))
 			)	
 			(if (or (whitespace-char cur-ch) (equalp i 0))
